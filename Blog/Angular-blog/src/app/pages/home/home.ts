4d694b4efa9a9d47cbf24626886a2286bd46bd24
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-08-11 13:35:46 -0300

    feat: Drawing the small card
    Creating the small card structure

--- Blog/Angular-blog/src/app/pages/home/home.ts
+++ Blog/Angular-blog/src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuTitle } from '../../components/menu-title/menu-title';
 import { BigCard } from '../../components/big-card/big-card';
+import { SmallCard } from '../../components/small-card/small-card';
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTitle, BigCard],
+  imports: [MenuTitle, BigCard, SmallCard],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
