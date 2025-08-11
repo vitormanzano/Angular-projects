fa0960215e884f5400df3ea721cdda05067afe46
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-08-11 13:54:57 -0300

    feat: Add the menu bar
    Drawing the menu bar structure

--- Blog/Angular-blog/src/app/pages/home/home.ts
+++ Blog/Angular-blog/src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MenuTitle } from '../../components/menu-title/menu-title';
 import { BigCard } from '../../components/big-card/big-card';
 import { SmallCard } from '../../components/small-card/small-card';
+import { MenuBar } from '../../components/menu-bar/menu-bar';
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTitle, BigCard, SmallCard],
+  imports: [MenuTitle, BigCard, SmallCard, MenuBar],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
