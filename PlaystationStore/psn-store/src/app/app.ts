550c2f0e367179e77599dbb49e6073248dbd5636
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-08-18 18:23:57 -0300

    feat: Adding the components and building the menu bar

--- PlaystationStore/psn-store/src/app/app.ts
+++ PlaystationStore/psn-store/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBar } from "./components/menu-bar/menu-bar";
+import { Home } from './pages/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBar, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ PlaystationStore/psn-store/src/app/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Card } from '../../components/card/card';
+
+@Component({
+  selector: 'app-home',
+  imports: [Card],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
